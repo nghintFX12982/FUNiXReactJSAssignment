65b53ba2282e98de7c019a1c7b5707ccc586764a
Nghi Nguyen <nghintFX12982@funix.edu.vn>
2021-11-17 22:57:43 +0700

    make list in StaffList component & fix image path

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Card, CardImg, CardBody, CardTitle, CardGroup } from "reactstrap";
 
 class StaffList extends Component {
   constructor(props) {
@@ -6,9 +7,24 @@ class StaffList extends Component {
   }
 
   render() {
+    const staffName = this.props.staffList.map((staff) => {
+      return (
+        <CardGroup className="col-12 col-md-6 col-lg-4">
+          <Card className="m-1" key={staff.id}>
+            <CardImg top src={staff.image} alt={staff.name} />
+            <CardBody>
+              <CardTitle>
+                {staff.name} - {staff.department.name}
+              </CardTitle>
+            </CardBody>
+          </Card>
+        </CardGroup>
+      );
+    });
+
     return (
       <div className="container">
-        <div className="row"></div>
+        <div className="row">{staffName}</div>
         <div className="row">
           <div className="col-12">
             Bấm vào hình nhân viên để xem thông tin chi tiết
