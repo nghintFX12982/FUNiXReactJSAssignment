2736bdc05ff3e06c1c5eeae56a125cdfc5edabbb
Nghi Nguyen <nghintFX12982@funix.edu.vn>
2021-11-18 08:37:05 +0700

    desgin drop down menu to choose website layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  UncontrolledDropdown,
+} from "reactstrap";
 import StaffList from "./components/StaffListComponent";
 import { STAFFS } from "./shared/staffs";
 
@@ -9,7 +17,12 @@ class App extends Component {
     super(props);
     this.state = {
       staffs: STAFFS,
+      layout: null,
     };
+  }
+
+  setLayout(colwidth) {
+    this.setState({ layout: colwidth });
   }
 
   render() {
@@ -18,6 +31,28 @@ class App extends Component {
         <Navbar dark color="info">
           <div className="container">
             <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+            <Nav>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle caret color="muted">
+                  Bố cục
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem onClick={() => this.setLayout(6)}>
+                    2 cột
+                  </DropdownItem>
+                  <DropdownItem onClick={() => this.setLayout(4)}>
+                    3 cột
+                  </DropdownItem>
+                  <DropdownItem onClick={() => this.setLayout(3)}>
+                    4 cột
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem onClick={() => this.setLayout(null)}>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
           </div>
         </Navbar>
         <StaffList staffList={this.state.staffs} />
