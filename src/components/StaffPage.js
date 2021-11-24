8c4f040374957aa027b692883b83d8644faa04ea
Nghi Nguyen <nghintFX12982@funix.edu.vn>
2021-11-24 10:03:17 +0700

    make filter feature work on staff page

--- src/components/StaffPage.js
+++ src/components/StaffPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -7,38 +7,18 @@ import {
   CardBody,
   CardTitle,
   FormGroup,
-  Label,
   Input,
 } from "reactstrap";
 import { Link, Route } from "react-router-dom";
 
 // ----- Container Component -----
 const RenderBreadcrumb = ({ staffList, match }) => {
-  function myFunc() {
-    const filterList = staffList.filter(
-      (staff) =>
-        staff.department.name === document.getElementById("mySelect").value
-    );
-  }
-
   return (
-    <React.Fragment>
-      <Breadcrumb tag="nav" listTag="div" className="col-12 col-md-8">
-        <BreadcrumbItem>
-          <Link to="/staff">Nhân Viên</Link>
-        </BreadcrumbItem>
-      </Breadcrumb>
-      <FormGroup className="col-12 col-md-4">
-        <Input type="select" name="select" id="mySelect" onChange={myFunc}>
-          <option value="default">Mặc định</option>
-          <option value="Finance">Bộ phận Finance</option>
-          <option value="HR">Bộ phận HR</option>
-          <option value="IT">Bộ phận IT</option>
-          <option value="Marketing">Bộ phận Marketing</option>
-          <option value="Sale">Bộ phận Sale</option>
-        </Input>
-      </FormGroup>
-    </React.Fragment>
+    <Breadcrumb tag="nav" listTag="div">
+      <BreadcrumbItem>
+        <Link to="/staff">Nhân Viên</Link>
+      </BreadcrumbItem>
+    </Breadcrumb>
   );
 };
 
@@ -56,22 +36,63 @@ const RenderStaff = ({ match, staff }) => {
 };
 
 // ----- Presentational Component -----
-function StaffPage({ match, staffList }) {
-  const staff = staffList.map((staff) => {
-    return (
-      <div className="col-6 col-md-4 col-xl-2 my-2">
-        <RenderStaff staff={staff} match={match} />
-      </div>
-    );
-  });
+function StaffPage(props) {
+  const [staffList, setStaffList] = useState(props.staffList);
+
+  function myFunc() {
+    let btnValue = document.getElementById("mySelect").value;
+    let filterList = [];
+
+    if (btnValue === "default") {
+      filterList = [...props.staffList];
+    } else {
+      filterList = props.staffList.filter(
+        (staff) => staff.department.name === btnValue
+      );
+    }
+
+    console.log(filterList);
+    setStaffList(filterList);
+  }
+
+  const Staff = () => {
+    const staff = staffList.map((staff) => {
+      return (
+        <div className="col-6 col-md-4 col-xl-2 my-2">
+          <RenderStaff staff={staff} match={props.match} />
+        </div>
+      );
+    });
+
+    return <div className="row">{staff}</div>;
+  };
 
   return (
     <React.Fragment>
       <div className="container">
         <div className="row">
-          <RenderBreadcrumb staffList={staffList} match={match} />
+          <div className="col-12 col-md-8">
+            <RenderBreadcrumb staffList={staffList} match={props.match} />
+          </div>
+          <div className="col-12 col-md-4">
+            <FormGroup>
+              <Input
+                type="select"
+                name="select"
+                id="mySelect"
+                onChange={myFunc}
+              >
+                <option value="default">Mặc định</option>
+                <option value="Finance">Bộ phận Finance</option>
+                <option value="HR">Bộ phận HR</option>
+                <option value="IT">Bộ phận IT</option>
+                <option value="Marketing">Bộ phận Marketing</option>
+                <option value="Sale">Bộ phận Sale</option>
+              </Input>
+            </FormGroup>
+          </div>
         </div>
-        <div className="row">{staff}</div>
+        <Route path={`${props.match.path}`} component={Staff} />
       </div>
     </React.Fragment>
   );
